08ef862e7b15e01ca97239280ef00370c873fd10
SamadhanTikone <samadhantikone003@gmail.com>
2024-12-01 11:37:55 +0530

    vercel file name

--- src/Components/Room.jsx
+++ src/Components/Room.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 export default function Room({url}) {
   return (
     <div className="w-[77%]">
-      <img src={url} alt=""  className=" w-full "/>
+      <img src={url} alt=""  loading="lazy" className=" w-full "/>
     </div>
   )
 }
